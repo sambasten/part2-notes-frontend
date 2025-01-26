e3b1c93ce6f082b205c89e8183ed7f90337769e8
sambasten <akinwunmi_s@yahoo.com>
2025-01-25 22:19:16 -0500

    fetch axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNum, setNewNum] = useState('')
   const [filterName, setFilterName] = useState('')
+  
+  useEffect(() => {
+    const promise = axios.get('http://localhost:3001/persons')
+    console.log('axios promise', promise)
+    promise.then(response => {
+      console.log('resp', response.data)
+      setPersons(response.data)
+    })
+    
+  }, [])
   
   const handleFiltering = (e) => {
     const filterVal = e.target.value
